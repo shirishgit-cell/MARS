35f137eb0b5680e3fbcee894df40bf38d1840cc1
shirishgit-cell <81236860+shirishgit-cell@users.noreply.github.com>
2025-10-29 23:38:06 +0530

    Update prep-project.sh

--- prep-project.sh
+++ prep-project.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # MAKE SURE GCP PROJECT IS SET
 # gcloud config set project PROJECT_ID
-gcloud config set project playground-s-11-2f33e9e8
+gcloud config set project playground-s-11-f1443822
 if [[ -z "${GOOGLE_CLOUD_PROJECT}" ]]; then
     echo "Project has not been set! Please run:"
     echo "   gcloud config set project PROJECT_ID"
