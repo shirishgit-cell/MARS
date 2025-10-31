368e09967e813195070c4376ebd0b8f9c36f2dae
shirishgit-cell <81236860+shirishgit-cell@users.noreply.github.com>
2025-10-31 10:19:03 +0530

    Use current GCP project in prep script
    Replace hardcoded project ID with dynamic retrieval.

--- streaming/prep-project-stream.sh
+++ streaming/prep-project-stream.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # MAKE SURE GCP PROJECT IS SET
-gcloud config set project playground-s-11-a14fae34
+gcloud config set project $(gcloud config get-value project)
 if [[ -z "${GOOGLE_CLOUD_PROJECT}" ]]; then
     echo "Project has not been set! Please run:"
     echo "   gcloud config set project PROJECT_ID"
